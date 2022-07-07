7f89f4ca371e3dc2afda725336d89b7cdc81d043
Victor <victorgpj25@gmail.com>
2022-07-07 16:40:43 -0300

    getUserOrders

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express"
 import authRouter from "./authRouter.js"
+import saleRouter from "./saleRouter.js"
 
 const router = express.Router()
 
 router.use(authRouter)
+router.use(saleRouter)
 
 export default router
